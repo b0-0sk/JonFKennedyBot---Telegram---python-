6252dc3ccc7cb51258a6543698cb9377f7ddd6bb
b0-0sk <arnaubosch2dam@gmail.com>
2019-10-09 18:08:21 +0200

    Wednesday 18:00

--- BotTelegram.py
+++ BotTelegram.py
@@ -2,20 +2,20 @@
 import telebot
 
 TOKEN = "891479632:AAEMeE7Ca15mkC0sVoY7g8F3I5F2MSktUxE"
-albondiga = telebot.TeleBot(TOKEN)	#create a new Telegram Bot object
-@albondiga.message_handler(commands=['start', 'help'])
+JohnFKennedy = telebot.TeleBot(TOKEN)	#create a new Telegram Bot object
+@JohnFKennedy.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	chatid = message.chat.id
 
 	print(message.chat.id)
-	albondiga.send_message(chatid, "Que pasa ")
-@albondiga.message_handler(func=lambda message: True)
+	JohnFKennedy.send_message(chatid, "Que pasa ")
+@JohnFKennedy.message_handler(func=lambda message: True)
 def echo_all(message):
 	chatid = message.chat.id
 	audio = ('/home/abosch/Escriptori/ReBoTa.mp3', " ")
-	albondiga.send_audio(chatid, audio)
+	JohnFKennedy.send_audio(chatid, audio)
 
-	if message.chat.type == "private":
-		albondiga.send_audio(chatid)
+	if message.chat.type == "group":
+		JohnFKennedy.send_audio(chatid, audio)
 
-albondiga.polling()
+JohnFKennedy.polling()

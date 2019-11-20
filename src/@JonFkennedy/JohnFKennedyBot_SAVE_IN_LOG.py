c6730da9587365faacc405d6724c21c78007ccf9
Arnau Bosch Gomez <56040620+b0-0sk@users.noreply.github.com>
2019-11-20 18:08:33 +0100

    Add files via upload

--- src/@JonFkennedy/JohnFKennedyBot_SAVE_IN_LOG.py
+++ src/@JonFkennedy/JohnFKennedyBot_SAVE_IN_LOG.py
@@ -7,15 +7,22 @@ from telebot import types # Tipos para la API del bot.
 
 import time # Librería para hacer que el programa que controla el bot no se acabe.
 
-from datetime import datetime
+from datetime import datetime, date
 
 import os.path as path
 from encodings import undefined
 
 import sys
 from atk import Document
+from cgitb import text
 reload(sys)
 sys.setdefaultencoding('utf8')
+
+import telebot 
+import ast 
+
+import types 
+
 
 
 
@@ -28,9 +35,8 @@ token = "891479632:AAEMeE7Ca15mkC0sVoY7g8F3I5F2MSktUxE" # token del bot.
 
 
 
-AYUDA = 'Puedes utilizar los siguientes comandos : \n\n/info - Guia para utilizar el bot\n/hola - Saludo del Bot \n/b0_0sk - Informacion sobre b0_0sk \n\n'
+AYUDA = 'Puedes utilizar los siguientes comandos : \n\n/info - Guia para utilizar el bot \n         · /search [keyword] [search dd/mm/yyyy hh/mm/ss]\n/hola - Saludo del Bot \n/b0_0sk - Informacion sobre b0_0sk \n\n'
 
-SET_KEYWORD = 'Introduiex la paraula que vols que introduiexi.\n'
 
 GRUPO_BOT = "-270997133" #Definimos que cuando pongamos la palabra grupo lo vincule con el Id del grupo donde nos encontremos.  Al meter el bot en un grupo, en la propia consola nos saldrá
 
@@ -68,60 +74,55 @@ def command_ayuda(messages): # Definimos una función que resuleva lo que necesi
 
     JohnFKennedyBot.send_message( cid, AYUDA) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
 
+@JohnFKennedyBot.message_handler(commands=['botones']) # Indicamos que lo siguiente va a controlar el comando '/ayuda'
 
-@JohnFKennedyBot.message_handler(commands=['info']) # Indicamos que lo siguiente va a controlar el comando '/info'
+def command_bt(messages): # Definimos una función que resuleva lo que necesitemos.
+
+    cid = messages.chat.id # Guardamos el ID de la conversación para poder responder.
+
+    
+    JohnFKennedyBot.send_message( cid, AYUDA) 
+
+
+
+@JohnFKennedyBot.message_handler(commands=['search']) # Indicamos que lo siguiente va a controlar el comando '/info'
 
 def command_info(messages): # Definimos una función que resuleva lo que necesitemos.
     
     cid = messages.chat.id # Guardamos el ID de la conversación para poder responder.
-    
     keyword = messages.text.split()
-    JohnFKennedyBot.send_message(cid, keyword[1])
-    documento = open( str(cid) +".txt")
-    linea = documento.readline()
     
-    while linea != "":
-      if keyword[1] in linea:
-          
-          JohnFKennedyBot.send_message(cid, )
-      linea = documento.readline()
+    if len(keyword) > 1:
+       
+        
+        #JohnFKennedyBot.send_message(cid, keyword[1])
+        documento = open( str(cid) +".txt")
+        linea = documento.readline()  
+        CountLines = 0
+        
+        while linea != "":
+          if keyword[1] in linea:
+              
+              InfoWords= linea.split("|")
+              #print ("Keyword" + keyword[1])
+              InfoWords[3] = InfoWords[3].replace(keyword[1],"*"+keyword[1]+"*")
+              
+              JohnFKennedyBot.send_message(cid, "    - User: ["+InfoWords[0] +"]\n    - Date: ["+ InfoWords[2]+"]\n    - Message: "+ InfoWords[3],parse_mode= 'Markdown')
+              CountLines = CountLines + 1
+          linea = documento.readline()
+        
+        JohnFKennedyBot.send_message(cid, "Ha habido " + str(CountLines) + " ocurrencias")
 
+        
+    else:
+        
+        JohnFKennedyBot.send_message(cid, "Porfavor, introduzca un argumento :\n  - /search [keyword]\n  - /search dd/mm/yyyy hh/mm/ss] ")
          
-           
-    #dato = documento.read() 
-        
-        
-    #JohnFKennedyBot.send_message( cid, dato)
-    #documento.close()
     
-    
-    
-@JohnFKennedyBot.message_handler(commands=['historial']) # Indicamos que lo siguiente va a controlar el comando '/set_channel' INUTIL
-
-def historial(messages): # Definimos una función que resuleva lo que necesitemos.
-
-    cid = messages.chat.id # Guardamos el ID de la conversación para poder responder.
-    
-    JohnFKennedyBot.send_chat_action(cid, 'typing') # Enviando ...
-    
-    JohnFKennedyBot.send_message( cid, SET_KEYWORD) # Nos devolvera el mensaje que previamente hemos escrito en la variable SET_CHANNEL.
-
-
-@JohnFKennedyBot.message_handler(commands=['search']) # Indicamos que lo siguiente va a controlar el comando '/set_channel'
-
-def search(messages): # Definimos una función que resuleva lo que necesitemos.
-
-    cid = messages.chat.id # Guardamos el ID de la conversación para poder responder.
-    
-    JohnFKennedyBot.send_chat_action(cid, 'typing') # Enviando ...
-    
-    JohnFKennedyBot.send_message( cid, SET_CHANNEL) # Nos devolvera el mensaje que previamente hemos escrito en la variable SET_CHANNEL.
-
-
 
 def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
-
-    now = datetime.now()
+  
+    now = datetime.now() # Variable para guardar la fecha del mensaje
     for word in messages: # Por cada dato 'm' en el dato 'messages'
              
         cid = word.chat.id # Guardamos el ID de la conversación para poder responder.  
@@ -136,15 +137,16 @@ def listener(messages): # Con esto, estamos definiendo una función llamada 'lis
         if cid > 0: # Conversacion privada
 
             clientName =  str(word.chat.first_name) # nom del chat
-
-            
+                        
             clientChat = str(word.chat)
 
             nombre_documento = str(cid)
 
-            mensaje = clientName + " [" + str(cid) + "|" + clientDatetime +"]: " + clientText     # Si 'cid' es positivo, usaremos 'm.chat.first_name' para el nombre.
+            mensaje = clientName + "|" + str(cid) + "|" + clientDatetime +"|" + clientText+"| "     # Si 'cid' es positivo, usaremos 'm.chat.first_name' para el nombre.
             
-            print (clientText)
+            
+            
+            
 
         elif cid < 0: # Conversacion grupal
 
@@ -152,7 +154,7 @@ def listener(messages): # Con esto, estamos definiendo una función llamada 'lis
             groupName = str(word.chat.title) # Name group
             clientName = str(word.from_user.first_name) #Name user
 
-            mensaje = clientName + " [" + str(cid) + "|" + clientDatetime +"]: " + clientText
+            mensaje = clientName + "|" + str(cid) + "|" + clientDatetime +"|" + clientText+"| "
             nombre_documento = str(cid)
             
             #for word in messages:
